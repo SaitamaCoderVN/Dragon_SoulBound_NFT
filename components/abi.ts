d5bca58203c9cb0ce161ec7bab0fbfa97e06fa83
root <root@anhitshare>
2024-05-14 18:05:44 +0700

    update abi

--- components/abi.ts
+++ components/abi.ts
@@ -399,6 +399,25 @@ export const abi = [
         "internalType": "address",
         "name": "owner",
         "type": "address"
+      }
+    ],
+    "name": "getSoulboundNFTs",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
       },
       {
         "internalType": "address",
