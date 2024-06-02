1810d8b000b754f4e151e06dbcec1531ef5619d6
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-06-03 00:39:20 +0700

    style: Update UI components

--- components/stepbox.tsx
+++ components/stepbox.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Stepbox({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className="bg-black text-white text-[0.85rem] font-semibold rounded-full py-1 px-3 flex justify-center items-center"
+      className="bg-dark-text text-white text-[0.85rem] text-nowrap font-semibold rounded-full py-1 px-3 flex justify-center items-center"
     >
       <p>{children}</p>
     </div>
